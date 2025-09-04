bd636a4c42aa0e97e22ed84f95f32f3d72a861cc
ielharra <moroccobrahem@gmail.com>
2025-09-04 23:08:09 +0100

    the main updated

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 mod assembler;
 mod vm;
 mod instructions;
-// mod utils;
-
+mod utils;
+mod disassembler;
 
 fn main() -> anyhow::Result<()> {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() != 2 {
+    if args.len() < 2 {
         eprintln!("Usage: {} <file.s>", args[0]);
         std::process::exit(1);
     }
     
-    assembler::run_file(&args[1])?;
+    let binary_code = assembler::run_file(&args[1]);
+    if let Ok(b) = binary_code {
+        utils::write_cor_file("player.cor", b)?
+    }
+
+    let dis = disassembler::Disassembler::read_cor_file("player.cor")?;
+    disassembler::writer::write_s_file(&dis, "player_disassembled.s")?;
+    println!("Disassembled file written!");
+    //write into file;
     Ok(())
 }
