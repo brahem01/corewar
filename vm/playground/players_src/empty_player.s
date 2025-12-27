ed6e0bcddf7bf582260bb865636212c6c8aeaaee
MOHAMED EL FIHRY <fihrylara@gmail.com>
2025-12-27 11:52:55 +0100

    remove the old vm

--- vm/playground/players_src/empty_player.s
+++
@@ -1,2 +0,0 @@
-.name "all instructions"
-.description "all possible instructions"
--- vm/playground/players_src/pierino.s
+++
@@ -1,6 +0,0 @@
-.name "pierino"
-.description "stay alive"
-    sti r1, %:a, %1
-a:	live %23
-    ld %0, r2
-	zjmp %:a
